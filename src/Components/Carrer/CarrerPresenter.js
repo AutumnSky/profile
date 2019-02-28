a1e549445d0901eceaa11f1105c60f92eafb5ced
MJ K <capu1004@gmail.com>
2019-02-28 20:09:11 +0900

    career

--- src/Components/Carrer/CarrerPresenter.js
+++ src/Components/Carrer/CarrerPresenter.js
@@ -1,3 +1,55 @@
 import React from 'react';
+import styled, { ThemeProvider } from 'styled-components';
+import theme from 'Theme';
 
-export default () => 'Carrer';
+const StyledUl = styled.ul``;
+
+const StyledLi = styled.li`margin-bottom: 40px;`;
+
+const ItemContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const ItemHeader = styled.span`
+  color: ${(props) => props.theme.main};
+  font-size: 22px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
+
+const ItemYear = styled.span`
+  color: ${(props) => props.theme.fontDesc};
+  font-size: 18px;
+  font-weight: 500;
+  margin-left: 20px;
+  margin-bottom: 10px;
+`;
+
+const ItemRole = styled.p`
+  color: ${(props) => props.theme.fontDesc};
+  font-size: 18px;
+  font-weight: 500;
+  margin-left: 20px;
+`;
+
+export default () => (
+  <ThemeProvider theme={theme}>
+    <StyledUl>
+      <StyledLi>
+        <ItemContainer>
+          <ItemHeader>Layer Lab</ItemHeader>
+          <ItemYear>December 2013 - July 2016</ItemYear>
+          <ItemRole>iOS, Android, Unity 2D</ItemRole>
+        </ItemContainer>
+      </StyledLi>
+      <StyledLi>
+        <ItemContainer>
+          <ItemHeader>Motion Blue</ItemHeader>
+          <ItemYear>May 2011 - May 2013</ItemYear>
+          <ItemRole>iOS, Android</ItemRole>
+        </ItemContainer>
+      </StyledLi>
+    </StyledUl>
+  </ThemeProvider>
+);
